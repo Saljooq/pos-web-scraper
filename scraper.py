30daee0df8d504dd527797bb917f5e438e393de4
Sal <altaf.saljooq@outlook.com>
2022-09-24 02:29:15 -0500

    tested and added two more columns - dept and course_code

--- scraper.py
+++ scraper.py
@@ -17,7 +17,7 @@ def main():
 
     file_ptr = open(OUTPUT_FILE, "a")
 
-    url = "https://catalog.iastate.edu/azcourses/hist/"
+    url = "https://catalog.iastate.edu/azcourses/com_s/"
     page = urlopen(url)
     html_bytes = page.read()
     html = html_bytes.decode("utf-8")
@@ -32,17 +32,23 @@ def main():
     print(f"list size {len(list_courses)}\n")
 
     # writing the headers of the file
-    file_ptr.write("course_num,credit_info,requirement,description\n")
+    file_ptr.write("dept,course_num,title,credit_info,requirement,description\n")
 
 
     for i, crse in enumerate(list_courses):
+        title_temp = crse.title.split(':')
+        temp_course_code_and_dept = sanitize_text(title_temp[0]).split()
+        crse.course_code = temp_course_code_and_dept.pop(-1)
+        crse.department = " ".join(temp_course_code_and_dept)
+
+        crse.title = title_temp[1]
         file_ptr.write(csv_row_formatter(crse))
 
 
     file_ptr.close()
 
 def csv_row_formatter(crse: Course) -> str:
-    return f"{surround_quotes(crse.title)},{surround_quotes(crse.credit_info)},{surround_quotes(crse.prereq)},{surround_quotes(crse.description)}\n"
+    return f"{surround_quotes(crse.department)},{surround_quotes(crse.course_code)},{surround_quotes(crse.title)},{surround_quotes(crse.credit_info)},{surround_quotes(crse.prereq)},{surround_quotes(crse.description)}\n"
 
 def surround_quotes(in_str: str)-> str:
     """surround input string with quotes"""
@@ -55,6 +61,12 @@ def surround_quotes(in_str: str)-> str:
         out_str = "None"
     return f"\"{out_str.strip()}\""
 
+def sanitize_text(in_str: str) -> str:
+    """clean the text from werid stuff and replace with spaces"""
+    out_str = in_str.replace('\n', ' ')
+    out_str = out_str.replace('\t', ' ')
+    out_str= out_str.replace('\xa0', ' ')
+    return out_str
 
 
 main()
